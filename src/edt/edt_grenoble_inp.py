2b3420531d2a95d73bb6fd98b544248ee5a9b642
Fruchix <fruchix@gmail.com>
2024-12-03 20:50:39 +0100

    style: format

--- src/edt/edt_grenoble_inp.py
+++ src/edt/edt_grenoble_inp.py
@@ -8,15 +8,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from enum import Enum
 
+
 class EdtGrenobleInpResources(Enum):
     Group2AA = 9314
+
 
 class EdtGrenobleInpOptions:
     DEFAULT_WIDTH = 793
     DEFAULT_HEIGHT = 851
     DEFAULT_ID_PIANO_DAY = "0,1,2,3,4,5,6"
     DEFAULT_RESOURCE = EdtGrenobleInpResources.Group2AA
-    
+
     # default values that should not be changed
     PROJECT_ID = 13
     DISPLAY_MODE = 1057855
@@ -34,24 +36,27 @@ class EdtGrenobleInpOptions:
         self.resource = self.DEFAULT_RESOURCE
         # get the current week by default
         self.set_week_starting_from_current(0)
-    
+
     def set_width(self, width: int) -> None:
         self.width = width
-    
+
     def set_height(self, height: int) -> None:
         self.height = height
-    
+
     def set_week_starting_from_current(self, week: int = 0) -> None:
         """Set the week starting from the current week.
 
         Args:
             week (int, optional): number of weeks to shift from the current. Defaults to 0.
         """
-        self.week = get_week_id(self.FIRST_WEEK_MONDAY_DATETIME, select_current_semaine()) + week
-    
+        self.week = (
+            get_week_id(self.FIRST_WEEK_MONDAY_DATETIME, select_current_semaine())
+            + week
+        )
+
     def set_resource(self, resource: EdtGrenobleInpResources) -> None:
         self.resource = resource
-    
+
     def get_dict(self):
         return {
             "idPianoWeek": self.week,
@@ -63,7 +68,7 @@ class EdtGrenobleInpOptions:
             "lunchName": self.LUNCH_NAME,
             "displayMode": self.DISPLAY_MODE,
             "showLoad": self.SHOW_LOAD,
-            "displayConfId": self.DISPLAY_CONFIG_ID
+            "displayConfId": self.DISPLAY_CONFIG_ID,
         }
 
 
@@ -76,15 +81,20 @@ class EdtGrenobleInpClient:
     def _init_cookies_and_identifier(self) -> None:
         driver = DriverFactory.build(
             DriverEnum.CHROME,
-            executable_path='/usr/bin/chromedriver',
-            options=["--headless"]
+            executable_path="/usr/bin/chromedriver",
+            options=["--headless"],
         )
 
-        driver.get("https://edt.grenoble-inp.fr/2024-2025/exterieur/jsp/standard/direct_planning.jsp")
+        driver.get(
+            "https://edt.grenoble-inp.fr/2024-2025/exterieur/jsp/standard/direct_planning.jsp"
+        )
 
         # set session cookies
         self.session.cookies.set("JSESSIONID", driver.get_cookie("JSESSIONID")["value"])
-        self.session.cookies.set("BIGipServer~ADE~pool_ade-inp-ro", driver.get_cookie("BIGipServer~ADE~pool_ade-inp-ro")["value"])
+        self.session.cookies.set(
+            "BIGipServer~ADE~pool_ade-inp-ro",
+            driver.get_cookie("BIGipServer~ADE~pool_ade-inp-ro")["value"],
+        )
 
         try:
             driver.implicitly_wait(2)
@@ -101,12 +111,12 @@ class EdtGrenobleInpClient:
 
             image = driver.find_element(By.XPATH, "/html/body/img")
             image_url = image.get_attribute("src")
-        except Exception as e: # bad practice to catch all exceptions
+        except Exception as e:  # bad practice to catch all exceptions
             # TODO: log instead of print
             print(e)
         finally:
             driver.quit()
-        
+
         self.identifier = image_url.split("identifier=")[1].split("&")[0]
 
     def download_edt(self, resource: EdtGrenobleInpResources, week: int):
@@ -119,17 +129,19 @@ class EdtGrenobleInpClient:
             # TODO: log instead of print
             print(e)
             return
-        except Exception as e: # bad practice to catch all exceptions
+        except Exception as e:  # bad practice to catch all exceptions
             # TODO: log instead of print
             print(e)
             return
 
         with open(f"data/edt-{resource.name}-{self.options.week}.png", "wb") as f:
             f.write(edt)
-    
+
     def get_edt(self) -> bytes:
         """Get the edt identified by the current EdtGrenobleInpOptions."""
         params = self.options.get_dict()
         params.update({"identifier": self.identifier})
-        
-        return self.session.get("https://edt.grenoble-inp.fr/2024-2025/exterieur/jsp/imageEt", params=params).content
+
+        return self.session.get(
+            "https://edt.grenoble-inp.fr/2024-2025/exterieur/jsp/imageEt", params=params
+        ).content
--- src/utils/datetime_utils.py
+++ src/utils/datetime_utils.py
@@ -1,16 +1,20 @@
 import datetime
 
+
 def select_current_semaine() -> datetime.date:
     """Select the first day of the current week. If it is the weekend, select the first day of the next week."""
 
     # get first day of the week
-    deb_sem = datetime.date.today() - datetime.timedelta(datetime.date.today().weekday())
+    deb_sem = datetime.date.today() - datetime.timedelta(
+        datetime.date.today().weekday()
+    )
 
     # if on the weekend, select the first day of the next week
     if datetime.date.today().weekday() > 4:
         deb_sem = deb_sem + datetime.timedelta(7)
 
     return deb_sem
+
 
 def get_week_id(start_date: datetime.date, selected_date: datetime.date) -> int:
     """Get the week id of the given date, starting from the start_date."""
